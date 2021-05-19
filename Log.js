005ecb13e6dad315fca7ac8bd077695b56a4be97
Paurvvik <82900881+Paurvvik@users.noreply.github.com>
2021-05-19 20:45:03 +0530

    Add files via upload

---
+++ Log.js
@@ -0,0 +1,15 @@
+class Log extends BaseClass {
+  constructor(x,y){
+    super(x,y,250,10);
+    this.image = loadImage("sprites/wood2.png");
+  }
+  display() {
+   
+    super.display();
+  }
+      
+  }
+  
+
+
+    
--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# C-28
-C-28
+# angryBirdsStage2StudentActivity
+Student Activity Link for Angry Birds Stage 2
